c2dbdc0da3024eb7bce5b40ed04d0ab72306a289
Khanh Nguyen <khanhnguyen2336@yahoo.com>
2022-11-22 04:09:04 +0000

    prep-javascript-arrays - Khanh Nguyen

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,14 @@
+var languages = ['html', 'css', 'javascript', 'react', 'node', 'postgres'];
+console.log('languages array', languages);
+
+const firstElement = languages[0];
+console.log(`The first element of the languages array is ${firstElement}.`);
+
+const thirdElement = languages[2];
+console.log(`The third element of the languages array is ${thirdElement}.`);
+
+const length = languages.length;
+console.log(`The length of the languages array is ${length}.`);
+
+const lastElement = languages[length - 1];
+console.log(`The last element of the languages array is ${lastElement}.`);
